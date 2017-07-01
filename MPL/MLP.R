005c47e5d70d5730667b6ab9a5996ee647c043d1
Felipe Cantagalli <felipe.cantagalli@gmail.com>
2017-07-01 12:11:39 -0300

    implementing ensemble

--- MPL/MLP.R
+++ MPL/MLP.R
@@ -1,65 +1,5 @@
 #Based on http://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/
 
-require(MLmetrics)
-
-#Load dataset
-setwd("~/Code/data_mining/MPL")
-#dataset <- read.table("seed", header = FALSE)
-
-#simpleNetwork <- createNetwork(2,1,2)
-
-#network$hiddenLayer[[1]]$weights <- c(0.13436424411240122, 0.8474337369372327)
-#network$hiddenLayer[[1]]$bias <- 0.763774618976614
-#network$hiddenLayer[[2]]$weights <- c(0.2550690257394217, 0.49543508709194095)
-#network$hiddenLayer[[2]]$bias <- 0.4494910647887381
-
-#network$outputLayer[[1]]$weights <- c(0.651592972722763, 0.7887233511355132)
-#network$outputLayer[[1]]$bias <- 0.0938595867742349
-#network$outputLayer[[2]]$weights <- c(0.02834747652200631, 0.8357651039198697)
-#network$outputLayer[[2]]$bias <- 0.43276706790505337
-
-
-#---------------------------------------------------------------
-set.seed(1)
-#dataset <- read.table("testdata", header= TRUE)
-dataset <- read.table("seed", header = FALSE)
-minmax <- datasetMinMax(dataset)
-dataset <- normalizeDataset(dataset, minmax)
-
-l_rate <- 0.4
-n_epoch <- 500
-n_hidden <- 5
-#trainedNetwork <- trainNetwork(network, dataset, 0.5, 20, n_outputs)
-scores <- evaluateAlgorithm(dataset, trainTestMPL, l_rate, n_epoch, n_hidden)
-#print('Scores: %s' % scores)
-#print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
-
-evaluateAlgorithm <- function (dataset, algorithm, ...) {
-  # 75% treinamento 25% teste
-  sample_size <- floor(0.75 * nrow(dataset))
-  train_set_indexes <- sample(seq_len(nrow(dataset)), size = sample_size)
-  train_set <- dataset[train_set_indexes,]
-  test_set <- dataset[-train_set_indexes,]
-  
-  predicted <- algorithm(train_set, test_set, ...)
-  actual <- test_set[,ncol(test_set)]
-  accuracy <- Accuracy(y_pred = predicted, y_true = actual)
-  print(accuracy)
-  return(accuracy)
-}
-
-trainTestMPL <- function(train, test, l_rate, n_epoch, n_hidden) {
-  n_inputs <- ncol(train) - 1
-  n_outputs <- length(unique(train[,ncol(train)]))
-  network <- createNetwork(n_inputs, n_hidden, n_outputs)
-  network <- trainNetwork(network, train, l_rate, n_epoch, n_outputs)
-  predictions <- sapply(1:nrow(test), function (rowIndex) {
-    predict(network, test[rowIndex,])
-  })
-  print(predictions)
-  return(predictions)
-}
-
 #We will organize layers as arrays of dictionaries and treat the whole network as an array of layers.
 
 datasetMinMax <- function(dataset) {
@@ -202,6 +142,9 @@ trainNetwork <- function(network, train_dataset, l_rate, n_epoch, n_outputs) {
       network <- updateNetworkWeights(network, row, l_rate)
     }
     printf('>epoch=%d, lrate=%.3f, error=%.3f',epoch, l_rate, sum_error)
+    if (sum_error < 3.00) {
+      break;
+    }
   }
   return(network)
 }
@@ -209,7 +152,6 @@ trainNetwork <- function(network, train_dataset, l_rate, n_epoch, n_outputs) {
 predict <- function (network, row) {
   network <- forwardPropagate(network, row)
   outputs <- getNetworkOutputs(network)
-  print(outputs)
   return (which.max(outputs))
 }
 
